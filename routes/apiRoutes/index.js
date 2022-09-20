f8fcf547475e5c4a0c8c65b5fce831f1854af362
Brandy Moore <brandymoore923@gmail.com>
2022-09-20 10:50:58 -0400

    added to routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,3 +2,20 @@ const router = require('express').Router();
 const {createNewNote, updateDb} = require('../../lib/notes');
 const { v4: uuidv4 } = require('uuid');
 const {notes} = require('../../db/db.json');
+
+//GET
+router.get('/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+  });
+
+//POST
+router.post('/notes', (req, res) => {
+    req.body.id = uuidv4();
+    const newNote = createNewNote(req.body, notes);
+    res.json(newNote);
+  });  
+//DELETE
+
+
+module.exports = router;
